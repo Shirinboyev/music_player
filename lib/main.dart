69f90255b31c2d712e566d1c315d84ecf394606a
Gayrat <gayratshirinboyev6707@gmail.com>
2023-04-10 11:08:15 +0500

    git pull

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,36 @@
 import 'package:flutter/material.dart';
-
+import 'package:music_player/screens/main_screen.dart';
+import 'package:music_player/screens/playing.dart';
+import 'package:music_player/screens/playlist.dart';
 void main() {
   runApp(const MainRoute());
 }
-
 class MainRoute extends StatefulWidget {
   const MainRoute({super.key});
-
   @override
   State<MainRoute> createState() => _MainRouteState();
 }
-
 class _MainRouteState extends State<MainRoute> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+        elevation: 0.0,
+        foregroundColor: Colors.black,
+        color: Colors.white,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+        )
+      ),
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
+      initialRoute: 'main_screen.dart',
       routes: {
-        '/': (context) => const Scaffold(
-              body: Center(
-                child: Text('Home'),
-              ),
-            ),
-        '/playlist': (context) => const Scaffold(
-              body: Center(
-                child: Text('Second'),
-              ),
-            ),
-        '/playing': (context) => const Scaffold(
-              body: Center(
-                child: Text('Third'),
-              ),
-            ),
+        'main_screen.dart': (context) => const MainPage(),
+        '/Playing': (context) => const PlayingPage(),
+        '/Playlist': (context) => const PlayListPage(),
       },
     );
   }
